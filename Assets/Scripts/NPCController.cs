619fac4cea0ed44760ee737bc0190fba709c7214
Thang <ggxxhay123@gmail.com>
2020-12-05 20:16:19 +0700

    NPC move coroutine

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System;
 
 public class NPCController : MonoBehaviour
 {
     public float Speed;
-    public Transform[] Waypoints;
+    public Transform[] Waypoints_Morning;
+    public Transform[] Waypoints_Afternoon;
+    public Transform[] Waypoints_Evening;
 
+    private Transform[] Waypoints_Current;
     private int currentWaypointIndex;
     private Rigidbody2D npcRigidbody2D;
     private Animator animator;
@@ -15,36 +20,54 @@ public class NPCController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         npcRigidbody2D = GetComponent<Rigidbody2D>();
+        Waypoints_Current = Array.Empty<Transform>();
+        StartCoroutine(Coroutine_SetWayPoints());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(currentWaypointIndex < Waypoints.Length)
+        if(currentWaypointIndex < Waypoints_Current.Length)
         {
             animator.SetBool("isMoving", true);
             MoveNPC();
         } else
         {
             animator.SetBool("isMoving", false);
+            animator.SetFloat("moveY", -1);
         }
     }
 
     void MoveNPC()
     {
-        var directionChange = (Vector2)(Waypoints[currentWaypointIndex].position - transform.position);
+        var directionChange = (Vector2)(Waypoints_Current[currentWaypointIndex].position - transform.position);
         animator.SetFloat("moveX", directionChange.x);
         animator.SetFloat("moveY", directionChange.y);
 
         transform.position = Vector2.MoveTowards(
             transform.position, 
-            Waypoints[currentWaypointIndex].position, 
+            Waypoints_Current[currentWaypointIndex].position, 
             Speed * Time.deltaTime
         );
 
-        if((Vector2)transform.position == (Vector2)Waypoints[currentWaypointIndex].position)
+        if((Vector2)transform.position == (Vector2)Waypoints_Current[currentWaypointIndex].position)
         {
             currentWaypointIndex++;
         }
+    }
+
+    IEnumerator Coroutine_SetWayPoints()
+    {
+        yield return new WaitForSeconds(3f);
+
+        currentWaypointIndex = 0;
+        Waypoints_Current = Waypoints_Morning;
+
+        yield return new WaitForSeconds(1f);
+        yield return new WaitUntil(() => !animator.GetBool("isMoving"));
+        yield return new WaitForSeconds(3f);
+
+        currentWaypointIndex = 0;
+        Waypoints_Current = Waypoints_Afternoon;
     }
 }
